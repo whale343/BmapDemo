7f2687162e69bab44a437e61e8e0331ea2151a09
whale-She <she15751@163.com>
2016-06-20 16:40:36 +0800

    Initial commit

---
+++ app/src/main/java/com/baidumap/she/whale/bmapdemo/MainActivity.java
@@ -0,0 +1,46 @@
+package com.baidumap.she.whale.bmapdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapView;
+
+public class MainActivity extends AppCompatActivity {
+    MapView mMapView = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        SDKInitializer.initialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+        //获取地图控件引用
+        mMapView = (MapView) findViewById(R.id.bmapView);
+        BaiduMap mBaiduMap = mMapView.getMap();
+        //普通地图
+//        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+        //卫星地图
+        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_SATELLITE);
+
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mMapView.onDestroy();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mMapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mMapView.onPause();
+    }
+}
